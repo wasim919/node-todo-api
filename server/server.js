85d4603725f79fa4919e4fb423ac6b5c6554fe99
= <=>
2018-08-19 16:03:49 +0530

    Add GET /todo/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
@@ -28,6 +29,21 @@ app.get('/todo', (req, res) => {
     });
   }, (err) => {
     res.status(400).send(err);
+  });
+});
+
+app.get('/todo/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(400).send();
+    }
+      res.status(200).send({todo});
+  }, (err) => {
+      res.status(404).send();
   });
 });
 
