cda2cba7eae96381a6fa0ee5979216bfd33274f0
= <=>
2018-08-19 18:03:45 +0530

    Add mongoLab configs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://tars:Tars@@ds125402.mlab.com:25402/todo332');
 
 module.exports = {mongoose};
