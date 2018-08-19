2dc397df3e375514558ace9d2701da919c82b17a
= <=>
2018-08-20 00:17:41 +0530

    Set MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGODB_URI || 'mongodb://localhost:27017');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017');
 
 module.exports = {mongoose};
