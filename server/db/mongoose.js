1879c8534f8c73b6dad1ef23ac064771533f6d94
= <=>
2018-08-19 23:48:33 +0530

    Add PATCH todo/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://tars:Tars@@ds125402.mlab.com:25402/todo332');
+mongoose.connect('mongodb://localhost:27017');
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
@@ -45,6 +46,48 @@ app.get('/todo/:id', (req, res) => {
       res.status(200).send({todo});
   }, (err) => {
       res.status(404).send();
+  });
+});
+
+
+app.delete('/todo/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)) {
+    console.log('hello');
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(id).then((doc) => {
+    if(!doc) {
+      return res.status(404).send();
+    }
+    res.status(200).send({doc});
+}).catch((err) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todo/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findOneAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo) {
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) => {
+    console.log(e);
+    res.status(400).send();
   });
 });
 
